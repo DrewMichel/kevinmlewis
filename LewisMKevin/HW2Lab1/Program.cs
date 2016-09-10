e83667a152c908a0f668558bc958e1caa702ea5a
KMLEWIS <KMLEWIS@STU_J_SP_16_22>
2016-09-10 09:59:07 -0700

    Week3 Demo1

--- LewisMKevin/HW2Lab1/Program.cs
+++ LewisMKevin/HW2Lab1/Program.cs
@@ -16,7 +16,9 @@ namespace HW2Lab1
             Console.WriteLine("Enter one number and press enter, then enter second number and press enter:");
 
             // declare two integer variables
-            int n1, n2, sum;
+            int n1, n2;
+            double sum;
+            double mult;
 
             string userInput;
             userInput = Console.ReadLine();
@@ -25,13 +27,25 @@ namespace HW2Lab1
 
             // calculate the sum of these two numbers
             sum = n1 + n2;
+            mult = n1 * n2;
 
             // print a message along with the sum of the two numbers
+
             Console.WriteLine("The sum of those two numbers is");
             Console.WriteLine(sum);
+            Console.WriteLine("The product of those two numbers is");
+            Console.WriteLine(Multiply(n1, n2));
+
+
+            Console.WriteLine("The division is {0}, sum {1}, mult {2}, div{0}.", sum / mult, sum, mult);
 
             //Just to pause the screen.
             Console.ReadLine();
+        }
+
+        static double Multiply(double x, double y)
+        {
+            return x * y;
         }
     }
 }
---
+++ LewisMKevin/Lecture3Demo1/Program.cs
@@ -0,0 +1,51 @@
+﻿/// @author: Kevin Lewis <kevin@kevinmlewis.com>
+/// Demonstration of Methods and String methods
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture3Demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nflTeam = "San Diego Chargers";
+
+            //Static cases
+            Console.WriteLine("Equals with the Broncos = {0}", nflTeam.Equals("Denver Broncos"));
+            Console.WriteLine("Equals with the Chargers = {0}", nflTeam.Equals("San Diego Chargers"));
+
+            //dynamic based on running the code.
+            Console.WriteLine("Enter the team that you would like to compare:");
+            string inputTeam = Console.ReadLine();
+            Console.WriteLine("Does the set string {0} equal the input string {1} = {2}",
+                nflTeam, inputTeam, nflTeam.Equals(inputTeam));
+
+            //Use the [] indexer method get the first and last characters.
+            char firstLetter = nflTeam[0];
+            char lastLetter = nflTeam[nflTeam.Length - 1];
+
+            Console.WriteLine("First letter ({0}), last letter ({1})", firstLetter, lastLetter);
+
+            //Substring method both 
+            Console.WriteLine("The {0} are from {1}.", nflTeam.Substring(10), nflTeam.Substring(0,9));
+
+            //use the index of method
+            Console.WriteLine("The index of the first space {0}.", nflTeam.IndexOf(" "));
+            Console.WriteLine("The index of Diego is {0}.", nflTeam.IndexOf("Diego"));
+
+            //new strings are created the original remains intact.
+            Console.WriteLine("UpperCase({0}), Original({1}).", nflTeam.ToUpper(), nflTeam);
+            string lowerNflTeam = nflTeam.ToLower();
+            Console.WriteLine("LowerCase({0}), Original({1}).", lowerNflTeam, nflTeam);
+
+
+            Console.ReadKey();
+        }
+    }
+}
