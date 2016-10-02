17c2778bc491199f0572e67f9e51c69b19d0241d
KMLEWIS <KMLEWIS@STU_J_SP_16_22>
2016-10-02 09:10:44 -0700

    try-catch-finally example.

---
+++ LewisMKevin/Lecture6Try/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture6Try
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] scoreList = new int[10];
+            try
+            {
+                Console.WriteLine("We are initializing the array.");
+                //let's initialize my array
+                for (int i = 0; i  <= 10; i++)
+                {
+                    scoreList[i] = 1;
+                }
+
+            }
+            //catch(IndexOutOfRangeException e) //a specific exception
+            //{
+            //    Console.WriteLine("Something bad with the array probably happened." + e);
+            //}
+            //this one must come after the specific ones. 
+            catch(Exception e) // this is the general exception
+            {
+                Console.WriteLine("Something bad happened." + e);
+            }
+            finally
+            {
+                Console.WriteLine("We are done");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
