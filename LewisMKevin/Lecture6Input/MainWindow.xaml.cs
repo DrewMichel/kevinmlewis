0866324c6bbb0d741de7f181d2bbb33d25f2781b
KMLEWIS <KMLEWIS@STU_J_SP_16_22>
2016-10-02 09:55:49 -0700

    Validate input solution

---
+++ LewisMKevin/Lecture6Input/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lecture6Input
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void checkButton_Click(object sender, RoutedEventArgs e)
+        {
+            outputTextBox.Text = "";
+            try
+            {
+                int value = Convert.ToInt32(numberTextBox.Text);
+                outputTextBox.Text = value.ToString();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Invalid input");
+                numberTextBox.Text = "";
+            }
+        }
+    }
+}
