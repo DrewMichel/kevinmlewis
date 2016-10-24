fc237377a1834d411e9d8442560a0447968f2cec
KMLEWIS <KMLEWIS@STU_J_SP_16_22>
2016-10-23 21:14:22 -0700

    Properties.

---
+++ LewisMKevin/Lecture8Properties/UnixTime.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture8Properties
+{
+    class UnixTime
+    {
+        private long _seconds;
+        public long Seconds
+        {
+            get
+            {
+                return _seconds;
+            }
+            set
+            {
+                //value is a keyword inside the set block 
+                _seconds = value;
+            }
+        }
+
+        //A property without a backing field.
+        public long Minutes
+        {
+            get
+            {
+                return _seconds / 60;
+            }
+        }
+
+        //Auto implemented property
+        public string Event { get; set; }
+
+    }
+}
