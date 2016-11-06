31ad54f77d76281065cd689196d187b8d1f6567f
KMLEWIS <KMLEWIS@STU_J_SP_16_22>
2016-11-06 15:52:57 -0800

    Lecture10PassByReference

---
+++ LewisMKevin/Lecture10PassByReference/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture10PassByReference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 10;
+
+            Console.WriteLine("Starting value = {0}", value);
+
+            PassByValue(value);
+            Console.WriteLine("After pass by value = {0}", value);
+
+            PassByReference(ref value);
+            Console.WriteLine("After pass by reference = {0}", value);
+
+            Console.ReadKey();
+        }
+
+        static void PassByValue(int value)
+        {
+            value = 15;
+        }
+
+        static void PassByReference(ref int value)
+        {
+            value = 20;
+        }
+    }
+}
