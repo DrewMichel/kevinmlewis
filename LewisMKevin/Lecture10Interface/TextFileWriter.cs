8a066a98e8b6d9bdca9ec3713348d159991cfcb7
KMLEWIS <KMLEWIS@STU_J_SP_16_22>
2016-11-06 17:09:21 -0800

    Interface Example

---
+++ LewisMKevin/Lecture10Interface/TextFileWriter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture10Interface
+{
+    public class TextFileWriter : IFileWriter
+    {
+        public string Extension
+        {
+            get { return ".txt"; }
+        }
+
+        public void Write(string filename)
+        {
+            //do your file writing here
+        }
+    }
+}
