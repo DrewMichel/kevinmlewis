1eafd424e6cbc25144755b0ed12b23c1222579e7
KMLEWIS <KMLEWIS@STU_J_SP_16_22>
2016-11-13 15:16:01 -0800

    Lecture 11 files and labs

---
+++ LewisMKevin/Lecture11Files/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture11Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter file = new StreamWriter("a.txt");
+
+            file.WriteLine("Hello");
+
+            Console.ReadKey();
+
+        }
+    }
+}
