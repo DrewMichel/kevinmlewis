6af3d2822b7400e70d799533ed38cc455694f9fc
KMLEWIS <KMLEWIS@STU_J_SP_16_22>
2016-11-13 14:48:50 -0800

    IComparable example.

--- LewisMKevin/Lecture10Interface/RtfFileWriter.cs
+++ LewisMKevin/Lecture10Interface/RtfFileWriter.cs
@@ -8,5 +8,17 @@ namespace Lecture10Interface
 {
     class RtfFileWriter : IFileWriter
     {
+        public string Extension
+        {
+            get
+            {
+                return ".rtf";
+            }
+        }
+
+        public void Write(string filename)
+        {
+            //write out the format
+        }
     }
 }
