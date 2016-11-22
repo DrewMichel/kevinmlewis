8479e5396b03b291d425140e42587fd6853dbd3b
KMLEWIS <KMLEWIS@STU_J_SP_16_22>
2016-11-21 18:57:10 -0800

    Class updates

--- LewisMKevin/Lecture12Generic/Program.cs
+++ LewisMKevin/Lecture12Generic/Program.cs
@@ -10,6 +10,30 @@ namespace Lecture12Generic
     {
         static void Main(string[] args)
         {
+            //PracticeList<String> myList = new PracticeList<string>();
+            //PracticeList<int> myInts = new PracticeList<int>();
+
+            //myList.Add("Hi");
+            //myInts.Add(56);
+
+            //Console.WriteLine(myList.GetItem(0));
+            //Console.WriteLine(myInts.GetItem(0));
+
+            Console.WriteLine(Average(1, 2, 3, 4));
+            int[] array = { 1, 2, 3, 4 };
+
+            Console.WriteLine(Average(array));
+
+            Console.ReadKey();
+        }
+
+        public static double Average(params int [] numbers)
+        {
+            double total = 0;
+            foreach (int number in numbers)
+                total += number;
+            return total / (double)numbers.Length;
+
         }
     }
 }
